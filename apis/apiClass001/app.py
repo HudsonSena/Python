0e3f484362a5493ebd408716a9f7d78949c0eae1
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-05-22 17:00:33 -0300

    Update app.py

--- apis/apiClass001/app.py
+++ apis/apiClass001/app.py
@@ -1,13 +1,42 @@
-# API - É um lugar paa disponibilizar recursos e/ou funcionalidades
-# 1. Objetivo - criar uma api de disponibilidade a consulta, criação, edição e exclusão de livros.
-# 2. URL base - localhost
-# 3. Endpoints - 
-    #- localhost/livros (GET)
-    #- localhost/livros (POST)
-    #- localhost/livros/id (GET)
-    #- localhost/livros/id (PUT)
-    #- locahhost/livros (DELETE)
-# 4. Quais recursos - Livros
+from flask import Flask, jsonify, request
 
-from flask import Flask
 app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'título': 'O senhor dos Anéis - A Sociedade do Anel',
+        'autor': 'J.R.R. Tolkien'
+    },
+    {
+        'id': 2,
+        'título': 'Harry Pottre e a Pedra Filosofal',
+        'autor': 'J.K. Howling'        
+    },
+    {
+        'id': 3,
+        'título': 'Habitos Atômicos',
+        'autor': 'James Clear'
+    },
+]
+
+# Consultar(Todos)
+@app.route('/livros', methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+
+# Consultar(id)
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+
+# Editar
+def editar_livro_por_id(id):
+    livros_alterado = request.get_json()
+    #continuar aqui
+
+# Excluir
+# Rodar
+app.run(port=5000, host='localhost', debug=True)
