ce3b2ee5c4416933e3fef63e9985589b014fc450
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-05-28 15:41:07 -0300

    Update app.py

--- apis/apiClass001/app.py
+++ apis/apiClass001/app.py
@@ -33,14 +33,30 @@ def obter_livro_por_id(id):
             return jsonify(livro)
 
 # Editar
-@aap.route('/livros/<int:id>', methods=['PUT'])
+@app.route('/livros/<int:id>', methods=['PUT'])
 def editar_livro_por_id(id):
-    livros_alterado = request.get_json()
+    livro_alterado = request.get_json()
     for indice, livro in enumerate(livros):
         if livro.get("id") == id:
             livros[indice].update(livro_alterado)
             return jsonify(livros[indice])
 
+# Criar
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
+
 # Excluir
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+    
+    return jsonify(livros)
+
 # Rodar
 app.run(port=5000, host='localhost', debug=True)
